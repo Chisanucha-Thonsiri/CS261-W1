226a364d3ef6b24ebcbdad9f23f84156f7ab0d35
Chisanucha <chisanucha.tho@dome.tu.ac.th>
2024-10-11 05:15:34 +0700

    Version1:Working TU REST API js request

--- public/index.js
+++ public/index.js
@@ -1,34 +1,41 @@
-document.getElementById('loginForm').addEventListener('submit', async function(event) {
-    event.preventDefault(); 
+function TU_REST_API(){
+    document.getElementById('loginForm').addEventListener('submit', async function(event) {
+        event.preventDefault(); 
+      
+    
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
 
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    const data = {
-        username: username,
-        password: password
-    };
-
-    try {
-
-        const response = await fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
-            method: 'POST',  
-            headers: {
-                'Content-Type': 'application/json'  
-            },
-            body: JSON.stringify(data)  
-        });
-        const result = await response.json();
-
-        if (response.ok) {
-            console.log('Login successful:', result);
-            alert('Login successful');
-        } else {
-            console.log('Error:', result);
-            alert('Login failed: ' + result.message);
+        const data = {
+            UserName: username,
+            PassWord: password
+        };
+    
+        try {
+            const applicationKey = 'TUd8d8ba4c157081a816c3b4cd5af7282b9380df4a4988a2bba327a9d6f71a61e845bf92ce3cd5e6187ecfb50173242dd3';  // Application Key, which is also the access token
+        
+            const response = await fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
+                method: 'POST',  
+                headers: {
+                    'Content-Type': 'application/json', 
+                    'Application-Key': applicationKey
+                },
+                body: JSON.stringify(data) 
+            });
+        
+            const result = await response.json();  
+        
+            if (response.ok) {
+                console.log('Login successful:', result);
+                document.getElementById("display-rp").innerText = `User Name: ${result.displayname_th}`
+                alert('Login successful');
+            } else {
+                console.log('Error:', result);
+                alert('Login failed: ' + result.message);
+            }
+        } catch (error) {
+            console.error('Error:', error);
         }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-});
+    });
+}
+
